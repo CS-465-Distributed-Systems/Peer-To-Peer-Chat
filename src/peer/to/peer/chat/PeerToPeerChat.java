1ebeb90b7c06811664c45383dae2bd6e7b544ac7
Terence Luther <tl377@nau.edu>
2014-09-12 13:49:13 -0700

    Framework

---
+++ src/peer/to/peer/chat/PeerToPeerChat.java
@@ -0,0 +1,34 @@
+package peer.to.peer.chat;
+
+public class PeerToPeerChat {
+
+    private String[] peerTable;
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public boolean connect(String ipAddress){
+        return true;
+    }
+    
+    public void updateTable(String inputTable){
+    
+    }
+    
+    public String[] getTable(){
+        return peerTable;
+    }
+
+    public void sendMessage(String input){
+    
+    }
+    
+    public void recieveMessage(String input){
+    
+    }
+    
+    public void updateCat(String input){
+    
+    }
+}
