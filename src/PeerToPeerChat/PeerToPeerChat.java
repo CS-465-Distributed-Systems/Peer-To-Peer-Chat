cc72d51337e130bd95b658ea43a15291876f5bb3
vtsyms <Veronika@Veronikas-MacBook-Pro.local>
2014-09-15 16:09:38 -0700

    Added server socket
    attempt to send and receive data

--- src/PeerToPeerChat/PeerToPeerChat.java
+++ src/PeerToPeerChat/PeerToPeerChat.java
@@ -1,11 +1,13 @@
 package PeerToPeerChat;
+import java.io.*;
+import java.net.*;
 
 public class PeerToPeerChat {
 
     private String[] peerTable;
 
     public static void main(String[] args) {
-        
+        DatagramSocket serverSocket = new DatagramSocket(9876);
     }
     
     public boolean connect(String ipAddress){
@@ -23,13 +25,31 @@ public class PeerToPeerChat {
     //String destination can designate who to send it to, listed out as option
     //a, b, c etc. where each letter stands for a recipient.
     public void sendMessage(String input, String destination){
-    
+        byte[] sendData = new byte[1024];
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        serverSocket.send(sendPacket);
     }
     
     public void recieveMessage(String input){
-    
+        byte[] receiveData = new byte[1024];
+        while(true){
+            byte[] reset = new byte[1024];
+            receiveData = reset;
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData());
+
+            sentence = sentence.trim();
+
+            System.out.println("RECEIVED: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+
+            System.out.println(sentence);
+        }
+
     }
-    
+
     public void updateCat(String input){
     
     }
